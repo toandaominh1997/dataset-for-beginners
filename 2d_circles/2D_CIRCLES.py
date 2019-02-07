399152dc465ef7845ace6b8ef089621424a2f94c
toandaominh1997 <toandaominh1997@gmail.com>
2019-02-07 07:09:43 +0700

    Add scaler in 2d_circle.py

--- 2d_circles/2D_CIRCLES.py
+++ 2d_circles/2D_CIRCLES.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd 
 from sklearn.model_selection import train_test_split
+
+
 from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.linear_model import LinearRegression
@@ -10,20 +12,16 @@ import matplotlib.pyplot as plt
 data = pd.read_csv('data.csv')
 y = data.iloc[:, 0]
 X = data.iloc[:, 1:]
-x1 = []
-y1 = []
-x2 = []
-y2 = []
-for i in range(0, y.shape[0]):
-    if(y.iloc[i]==1.0):
-        x1.append(X.iloc[i, 0])
-        y1.append(X.iloc[i, 1])
-    else:
-        x2.append(X.iloc[i, 0])
-        y2.append(X.iloc[i, 1])
 
+# Scale datasets
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+X = pd.DataFrame(scaler.fit_transform(X))
 
+# from sklearn.preprocessing import Normalizer
 
+# normalizer = Normalizer()
+# X = pd.DataFrame(normalizer.fit_transform(X))
 X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
 
 lr = LogisticRegression()
@@ -31,7 +29,7 @@ svc = SVC(C=20.0)
 #lr.fit(X_train, y_train)
 svc.fit(X_train, y_train)
 lr.fit(X_train, y_train)
-# print("Training accuracy: ", accuracy_score(y_val, svc.predict(X_val)))
+print("Accuracy: {:.2}, {:.2}".format(accuracy_score(y_train, svc.predict(X_train)), accuracy_score(y_val, svc.predict(X_val))))
 
 h = .02
 
